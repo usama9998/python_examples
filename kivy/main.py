953b7775d9b5aca5f0cfe45e78f8e8e3028c83d6
usama9998 <95363051+usama9998@users.noreply.github.com>
2021-12-11 22:44:58 +0500

    Update main.py

--- kivy/main.py
+++ kivy/main.py
@@ -1,1 +1,25 @@
+from kivy import *
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
+
+class TheLabApp(App):
+    pass
+
+######## Boxlayout widgets are relative    ############
+
+class BoxLayOutExample(BoxLayout):
+    pass
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+
+
+
+
+TheLabApp().run()
+
 
